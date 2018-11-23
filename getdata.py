4666a85a79a423b3f7832bb670b48c527c46be41
Carson Reid <cbgreid@uvic.ca>
2018-11-22 23:58:05 -0800

    Fixed up stats by year grabbing function, pretty sure api is down but oh well..

--- getdata.py
+++ getdata.py
@@ -1,8 +1,7 @@
 import requests as req
 import json
-
-baseURL = 'https://statsapi.web.nhl.com/api/v1/'
-r = req.get(baseURL+'teams')
+baseURL = "https://statsapi.web.nhl.com/api/v1/"
+r = req.get(baseURL+"teams")
 parsed = json.loads(r.text)
 teamIDlist = []
 for team in parsed["teams"]:
@@ -19,17 +18,29 @@ for (teamID, teamName) in teamIDlist:
         playerList.append(player)
     teamPlayerDict[teamID] = (teamName, teamID, playerList)
 
-for (teamID, teamName) in teamIDlist:
-    (teamName, teamID, playerList) = teamPlayerDict[teamID]
-    #print(teamName+" - "+str(teamID))
-    for player in playerList:
-        pass
-     #   print(player)
+#for (teamID, teamName) in teamIDlist:
+#    (teamName, teamID, playerList) = teamPlayerDict[teamID]
+#    print(teamName+" - "+str(teamID))
+#    for player in playerList:
+#        pass
+#        print(player)
 
     #print()
+
+
 def getYearlyStats(playerID, startYear, endYear):
-    for i in range(startYear, endYear+1):
-        print(i)
+    statsByYear = []
+    for year in range(startYear, endYear+1):
+        response = req.get(baseURL+"people/"+str(playerID)+"/stats?stats=statsSingleSeason&season="+str(year)+str(year+1))
+        print(baseURL+"people/"+str(playerID)+"/stats?stats=statsSingleSeason&season="+str(year)+str(year+1))
+        if (response.status_code == 200): 
+            parsed = json.loads(response.text)
 
-getYearlyStats(8477179, 2016, 2018)
+            print(parsed) 
+            if (len(parsed["stats"][0]["splits"]) > 0):
+                statsByYear.append((year, parsed["stats"][0]["splits"][0]))
+    return statsByYear
 
+stats = getYearlyStats(8477179, 2010, 2018)
+print(stats)
+#not working right now, pretty sure the API endpoint is down..
